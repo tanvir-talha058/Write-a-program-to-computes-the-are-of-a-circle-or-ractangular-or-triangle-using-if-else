21cf2bc35919c6ea4e3fd6d88874518487eabab0
Tanvir Ahmed <110832885+tanvir-talha058@users.noreply.github.com>
2024-11-07 23:47:03 +0600

    Update tanvir if else.c

--- tanvir if else.c
+++ tanvir if else.c
@@ -4,20 +4,25 @@ int main()
 {
 	float area, radius, width, length, a, b, c,s;
 	int choice;
+	
 	printf("Choose an Option to compute:\n ");
 	printf("1: Area of the Circle\n ");
 	printf("2: Area of the Rectangle\n ");
 	printf("3: Area of the Triangle\n ");
 	scanf("%d", &choice);
+	
 	if(choice==1)
+		
 	{
 		printf("Enter the radius of the Circle: ");
 		scanf("%f", &radius);
 		area = 3.1416*radius*radius;
 		printf("The area of the circle with radius %f is:", radius, area);
 	}
+	
 	if(choice ==2)
 	{
+		
 		printf("Enter the Width of the rectangle: ");
 		scanf("%f", &width);
 		printf("Enter the length of the rectangle: ");
@@ -25,6 +30,7 @@ int main()
 		area = width*length;
 		printf("The area of the rectangle is:%f", area);
 	}
+	
 	if(choice ==3)
 	{
 		printf("Enter the 3 sides of triangle):  ");
@@ -33,5 +39,6 @@ int main()
 		area =(s*(s-a)*(s-b)*(s-c));
 		printf("The area of the triangle is:%f", area);
 	}
+	
     return 0;
 }
